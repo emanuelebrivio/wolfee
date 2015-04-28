171cde8e435af39100084aafd3e1afa494230485
Emanuele Brivio <emanuele@plasticpanda.com>
2015-04-28 17:14:32 +0200

    Try new AJAX parameters

--- static/js/main.js
+++ static/js/main.js
@@ -16,9 +16,8 @@
   
   
   reqwest({
-      url: 'https://www.kimonolabs.com/api/8x3ce3zc?apikey=9e9e30f3542ec789be4de05d5c9f16e9'
-    , type: 'json'
-    , method: 'post'
+      url: 'http://www.kimonolabs.com/api/8x3ce3zc?apikey=9e9e30f3542ec789be4de05d5c9f16e9'
+    , type: 'jsonp'
     , contentType: 'application/json'
     , crossOrigin: true
     , error: function (err) { }
